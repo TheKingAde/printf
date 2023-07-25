c22fc56f96868d3083fe5f44f8b2118853390014
root <kibet9827>
2023-07-25 11:55:44 -0700

    _printint.c

--- _printint.c
+++ _printint.c
@@ -1,15 +1,1 @@
-#include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-/**
- * print_octal - print an unsigned number to octal
- * @num: integer
- * @buffer: pointer
- *
- */
-int print_octal(unsigned int num, char *buffer)
-{
-	int i;
-	unsigned int num;
-	int 
+ 
